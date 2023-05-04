9607cf400467dbdda4b14490da5140bb20a4ef91
lizhiquan <842048365@qq.com>
2023-05-04 10:02:58 +0800

    fix: 合并冲突

--- src/hooks/useHandleApiRes.ts
+++ src/hooks/useHandleApiRes.ts
@@ -1,18 +1,11 @@
 export function useHandleApiRes<T = any>(promise: PromiseLike<any>) {
   return promise.then((res) => {
-    const { isFinished, data, abort, aborted, canAbort, statusCode, execute } =
-      res;
+    const { data } = res;
     const { code, message, data: resData } = unref(data) || {};
     return {
-      isFinished,
       code,
       message,
       data: resData as T,
-      abort,
-      aborted,
-      canAbort,
-      statusCode,
-      execute,
     };
   });
 }
--- src/service/api/user.ts
+++ src/service/api/user.ts
@@ -1,10 +1,16 @@
 import { Request } from '@/service';
-import { ILogin } from '../types';
+import { ILogin } from '../types/user';
 import { User } from './index';
 
 export function Login(data: ILogin) {
   return Request.post({
     url: User.LOGIN,
     data,
+  });
+}
+
+export function getUserInfo() {
+  return Request.get({
+    url: User.INFO,
   });
 }
