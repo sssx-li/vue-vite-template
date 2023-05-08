6e2f5d900ed0c3563f3b2588867d230141a0d305
lizhiquan <842048365@qq.com>
2023-05-08 15:11:11 +0800

    perf: mock数据优化

--- src/mock/types.ts
+++ src/mock/types.ts
@@ -1,24 +1,23 @@
-import { Response } from 'miragejs';
-import { HandlerOptions } from 'miragejs/server';
+import { Response, Request } from 'miragejs';
+import { HandlerOptions, RouteHandler } from 'miragejs/server';
 
 import { AnyRegistry } from 'miragejs/-types';
 import Schema from 'miragejs/orm/schema';
 
 export type TMethods =
   | 'get'
+  | 'put'
   | 'post'
   | 'patch'
-  | 'put'
+  | 'delete'
   | 'options'
-  | 'del'
-  | 'head'
-  | 'delete';
+  | 'head';
 
 export type TApis = 'user';
 export interface IMock {
   method: TMethods;
   url: string;
-  response?: () => void;
-  handler?: (schema: Schema<AnyRegistry>, request: Request) => Response;
+  response?: (schema: Schema<AnyRegistry>, request: Request) => any;
+  handler?: RouteHandler<AnyRegistry, Response>;
   options?: HandlerOptions;
 }
