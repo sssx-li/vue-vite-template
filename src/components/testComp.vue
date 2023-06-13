1f3fbb0df733df8087b05df10cb41c84cc9a2d50
lizhiquan <842048365@qq.com>
2023-06-13 18:20:51 +0800

    perf: 更新components组件命名方式

---
+++ src/components/testComp.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex-inline p4px mx14px b b-solid b-#ccc">测试组件</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/tsxComp.tsx
@@ -0,0 +1,7 @@
+export default defineComponent({
+  setup() {
+    return () => (
+      <div class="p10px inline-block b b-solid b-#f60">Hello TSX</div>
+    );
+  },
+});
