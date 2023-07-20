bca118c2f7cb685ba6243db497ed8c0248c00acc
lizhiquan <842048365@qq.com>
2023-07-20 12:08:39 +0800

    perf: 优化$t在template中的参数提示

--- typing/global-properties.d.ts
+++ typing/global-properties.d.ts
@@ -1,8 +1,12 @@
 import { Component } from 'vue';
 import type { IFilters } from '../src/filters/types';
 
+import { ObjKeysToUnion } from '@/typing';
+import { TLangConfig } from '@/i18n/lang/en';
+
 declare module 'vue' {
   interface ComponentCustomProperties extends Component {
     $filters: IFilters;
+    $t(key: ObjKeysToUnion<TLangConfig>): string;
   }
 }
