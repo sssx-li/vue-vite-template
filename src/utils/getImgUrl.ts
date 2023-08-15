09650359e50e0c66ea9a80c13eb920c216d7d83f
lizhiquan <842048365@qq.com>
2023-08-15 18:25:28 +0800

    feat: 图片资源路径函数增加传入类型签名

--- src/utils/getImgUrl.ts
+++ src/utils/getImgUrl.ts
@@ -1,3 +1,5 @@
-export function getImgUrl(str: string) {
+import { imgBasePaths } from '../../generate';
+
+export function getImgUrl(str: (typeof imgBasePaths)[number]) {
   return new URL(`../assets/imgs/${str}`, import.meta.url).href;
 }
