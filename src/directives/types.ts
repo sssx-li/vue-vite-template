b68ff10cf7fec0b2495143fb0a09abeb81605b3e
lizhiquan <842048365@qq.com>
2023-08-29 09:21:11 +0800

    perf: 优化防抖节流el类型定义，增加__fn__函数

--- src/directives/types.ts
+++ src/directives/types.ts
@@ -2,6 +2,10 @@ import type { Directive } from 'vue';
 
 type EventTypes = 'click' | 'input';
 
+export interface ELType extends HTMLElement {
+  __fn__: () => any;
+}
+
 export interface Directives {
   vFocus: Directive;
   vDebounce: Directive<
@@ -23,11 +27,12 @@ export interface Directives {
 }
 
 export type Keys = keyof Directives;
-type DirectiveName<T extends Keys> = T extends `v${infer V}`
+
+type LowerDirectiveName<T extends Keys> = T extends `v${infer V}`
   ? Lowercase<V>
   : never;
 
 export interface DirectiveOptions<T extends Keys> {
-  name: DirectiveName<T>;
+  name: LowerDirectiveName<T>;
   directive: Directives[T];
 }
