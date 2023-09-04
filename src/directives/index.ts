33d1fee3f06add438a4ce7b2daf51950a8c9865e
lizhiquan <842048365@qq.com>
2023-09-04 09:39:02 +0800

    perf: 优化自定义指令代码;移除无用组件

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -4,7 +4,10 @@ import type { DirectiveOptions, Keys } from './types';
 export default async function directives(app: App) {
   const files = import.meta.glob('./modules/*.ts');
   for (const key in files) {
-    const file: DirectiveOptions<Keys> = ((await files[key]()) as any).default;
-    app.directive(file.name, file.directive);
+    const file: any = await files[key]();
+    if (file) {
+      const direct = file.default as DirectiveOptions<Keys>;
+      app.directive(direct.name, direct.directive);
+    }
   }
 }
