9cec1552654ca7681c4cf32c4245488e0272e9c4
lizhiquan <842048365@qq.com>
2023-09-26 17:10:43 +0800

    perf: vite配置分包

--- vite.config.ts
+++ vite.config.ts
@@ -104,4 +104,14 @@ export default defineConfig({
   optimizeDeps: {
     include: ['vue', 'vue-router', 'pinia', 'vue-i18n', '@vueuse/core'],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vue: ['vue', 'vue-router', 'vue-i18n', '@vueuse/core'],
+          pinia: ['pinia'],
+        },
+      },
+    },
+  },
 });
